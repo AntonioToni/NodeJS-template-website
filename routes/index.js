3b45d79b6c3a117ed766df7775041880dc19739c
Antonio <antonio.gaming10@gmail.com>
2022-03-06 03:27:54 +0100

    Reordering for simpler overview

--- routes/index.js
+++ routes/index.js
@@ -13,35 +13,52 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
 ); 
 
 router.get('/', function(req, res, next) {
-  res.render('index', {page_name: "index", title: '4mat' });
+  res.render('index', {
+    page_name: "index", 
+    title: '4mat'
+  });
 });
 
 router.get('/login', function (req, res) {
-  res.render('login', {page_name: "login"});
+  res.render('login', {
+    page_name: "login"
+  });
 });
 router.get('/register', function (req, res) {
-  res.render('register', {page_name: "register"});
+  res.render('register', {
+    page_name: "register"
+  });
 });
 
 // Pages
 router.get('/news', function (req, res) {
-  res.render('news', {page_name: "news"});
+  res.render('news', {
+    page_name: "news"
+  });
 });
 
 router.get('/about', function (req, res) {
-  res.render('about', {page_name: "about"});
+  res.render('about', {
+    page_name: "about"
+  });
 });
 
 router.get('/login', function (req, res) {
-  res.render('login', {page_name: "login"});
+  res.render('login', {
+    page_name: "login"
+  });
 });
 
 router.get('/news-template', function (req, res) {
-  res.render('news-template', {page_name: "_news"});
+  res.render('news-template', {
+    page_name: "_news"
+  });
 });
 
 router.get('/profile', function (req, res) {
-  res.render('profile', {page_name: "_profile"});
+  res.render('profile', {
+    page_name: "_profile"
+  });
 });
 
 
