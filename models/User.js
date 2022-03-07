c57afa4887b2b1c6ce3373f6b0cc10fca9ff1846
Antonio <antonio.gaming10@gmail.com>
2022-03-08 00:26:31 +0100

    Added description for user

--- models/User.js
+++ models/User.js
@@ -27,6 +27,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  description: {
+    type: String,
+    required: true
+  },
   admin: {
     type: Boolean,
     default: false
