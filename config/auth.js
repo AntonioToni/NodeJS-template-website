db7bec9187ddeffaaf84507fdd4b9acb736ffcdb
Antonio <antonio.gaming10@gmail.com>
2022-03-08 02:42:21 +0100

    Removed unecessary code

--- config/auth.js
+++ config/auth.js
@@ -13,12 +13,3 @@ module.exports = {
     res.redirect('/dashboard');      
   }
 };
-
-function loggedIn(req, res, next) {
-  if (req.user) {
-    return true;
-  }
-  else{
-    return false;
-  }
-}
